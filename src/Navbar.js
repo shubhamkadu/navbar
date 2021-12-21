442d0e537aa4ef777bdabd00963a2b1feae79877
shubhamkadu <shubhampkadu@gmail.com>
2021-12-21 16:10:49 -0600

    navbar

---
+++ src/Navbar.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect, useRef } from "react";
+import { FaBars, FaTwitter, FaBitcoin } from "react-icons/fa";
+import { links, social } from "./data";
+
+function Navbar() {
+    const [showLinks,setShowLinks] = useState(false)
+    const linksContainerRef = useRef(null)
+    const linksRef = useRef(null)
+
+    useEffect(()=>{
+     const linksHeight = linksRef.current.getBoundingClientRect().height
+        if(showLinks){
+            linksContainerRef.current.style.height = `${linksHeight}px`
+        } else{
+            linksContainerRef.current.style.height='0px'
+        }
+    },[showLinks])
+  return (
+    <nav>
+      <div className="nav-center">
+        <div className="nav-header">
+          <FaBitcoin size="1x" className="logo" />
+          <button className="nav-toggle">
+            <FaBars onClick={() => setShowLinks(!showLinks)} />
+          </button>
+        </div>
+        <div className="links-container" ref={linksContainerRef}>
+          <ul className="links" ref={linksRef}>
+            {links.map((link) => {
+              const { id, url, text } = link;
+              return (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <ul className="social-icons">
+          {social.map((socialicon) => {
+            const { url, id, icon } = socialicon;
+            return (
+              <li key={id}>
+                <a href={url}>{icon}</a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ src/data.js
@@ -0,0 +1,53 @@
+import React from "react";
+import {
+  FaFacebook,
+  FaLinkedin,
+  FaTwitter,
+} from "react-icons/fa";
+import { AiFillInstagram } from "react-icons/ai";
+
+export const links = [
+  {
+    id: 1,
+    url: "/",
+    text: "home",
+  },
+  {
+    id: 2,
+    url: "/about",
+    text: "about",
+  },
+  {
+    id: 3,
+    url: "/projects",
+    text: "projects",
+  },
+  {
+    id: 4,
+    url: "/contact",
+    text: "contact",
+  },
+];
+
+export const social = [
+  {
+    id: 1,
+    url: "https://www.facebook.com/",
+    icon: <FaFacebook />,
+  },
+  {
+    id: 2,
+    url: "https://www.twitter.com",
+    icon: <FaTwitter />,
+  },
+  {
+    id: 3,
+    url: "https://www.linkedin.com",
+    icon: <FaLinkedin />,
+  },
+  {
+    id: 4,
+    url: "https://www.instagram.com/",
+    icon: <AiFillInstagram  />,
+  },
+];
